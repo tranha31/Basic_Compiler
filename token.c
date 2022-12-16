bb54df01318ecd0b3f1da6ad0e7c5ca966fc7a6e
tranbamanh229289 <tranbamanhlovevietnam@gmail.com>
2022-12-17 04:37:11 +0700

    identity keyword lowercase uppercase

--- token.c
+++ token.c
@@ -13,31 +13,31 @@ struct {
   TokenType tokenType;
 } 
 keywords[KEYWORDS_COUNT] = {
-  {"PROGRAM", KW_PROGRAM},
-  {"CONST", KW_CONST},
-  {"TYPE", KW_TYPE},
-  {"VAR", KW_VAR},
-  {"INTEGER", KW_INTEGER},
-  {"CHAR", KW_CHAR},
-  {"ARRAY", KW_ARRAY},
-  {"OF", KW_OF},
-  {"FUNCTION", KW_FUNCTION},
-  {"PROCEDURE", KW_PROCEDURE},
-  {"BEGIN", KW_BEGIN},
-  {"END", KW_END},
-  {"CALL", KW_CALL},
-  {"IF", KW_IF},
-  {"THEN", KW_THEN},
-  {"ELSE", KW_ELSE},
-  {"WHILE", KW_WHILE},
-  {"DO", KW_DO},
-  {"FOR", KW_FOR},
-  {"TO", KW_TO}
+  {"Program", KW_PROGRAM},
+  {"Const", KW_CONST},
+  {"Type", KW_TYPE},
+  {"Var", KW_VAR},
+  {"Integer", KW_INTEGER},
+  {"Char", KW_CHAR},
+  {"Array", KW_ARRAY},
+  {"Of", KW_OF},
+  {"Function", KW_FUNCTION},
+  {"Procedure", KW_PROCEDURE},
+  {"Begin", KW_BEGIN},
+  {"End", KW_END},
+  {"Call", KW_CALL},
+  {"If", KW_IF},
+  {"Then", KW_THEN},
+  {"Else", KW_ELSE},
+  {"While", KW_WHILE},
+  {"Do", KW_DO},
+  {"For", KW_FOR},
+  {"To", KW_TO}
 };
 
 int keywordEq(char *kw, char *string) {
   while ((*kw != '\0') && (*string != '\0')) {
-    if (*kw != toupper(*string)) break;
+    if (*kw != *string) break;
     kw ++; string ++;
   }
   return ((*kw == '\0') && (*string == '\0'));
